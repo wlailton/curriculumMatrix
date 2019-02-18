19ee1ade903aa6b55a317c413c98c75cf51126b5
wlailton <wlailton@gmail.com>
2019-02-17 23:09:18 -0300

    Home page.

--- curriculumMatrix-spa/src/app/app.module.ts
+++ curriculumMatrix-spa/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './route/app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { CourseComponent } from './course/course/course.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    CourseComponent
+    CourseComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
